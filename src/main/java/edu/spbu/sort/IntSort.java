77e63a0e97d57c1ddc58909d61fbc4924035a439
st070041 <misha530530@gmail.com>
2019-09-19 12:03:10 +0300

    qSort 19.09.2019

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,8 +8,36 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+  public static void sort (int[] array) {
+      if (array.length == 0) return;
+    queSort(array, 0, array.length - 1);
+    return;
+  }
+
+  static int separate(int[] array, int begin, int end) {
+    int opora = end;
+
+    int counter = begin;
+    for (int i = begin; i < end; i++) {
+      if (array[i] < array[opora]) {
+        int t = array[counter];
+        array[counter] = array[i];
+        array[i] = t;
+        counter++;
+      }
+    }
+    int t = array[opora];
+    array[opora] = array[counter];
+    array[counter] = t;
+
+    return counter;
+  }
+
+  public static void queSort(int[] array, int begin, int end) {
+    if (end <= begin) return;
+    int opora = separate(array, begin, end);
+    queSort(array, begin, opora-1);
+    queSort(array, opora+1, end);
   }
 
   public static void sort (List<Integer> list) {
