74a18ea9f057e78bd92f9d990c94a6e67ecbf54d
mishamishamisha123 <misha530530@gmail.com>
2019-10-09 22:32:15 +0300

    Dense matrix: multy and equals

--- src/main/java/edu/spbu/matrix/DenseMatrix.java
+++ src/main/java/edu/spbu/matrix/DenseMatrix.java
@@ -10,18 +10,19 @@ import java.util.LinkedList;
 /**
  * Плотная матрица
  */
-public class DenseMatrix implements Matrix
-{
+public class DenseMatrix implements Matrix {
   double[][] matrixA;
   int width;
   int height;
+
   /**
    * загружает матрицу из файла
+   *
    * @param fileName
    */
   public DenseMatrix(String fileName) {
     LinkedList<double[]> matrix = new LinkedList<>();
-    try(BufferedReader r = new BufferedReader(new FileReader(fileName))) {
+    try (BufferedReader r = new BufferedReader(new FileReader(fileName))) {
       String s;
       while ((s = r.readLine()) != null) {
         String[] a = s.split(" ");
@@ -32,12 +33,13 @@ public class DenseMatrix implements Matrix
       e.printStackTrace();
     }
     matrixA = new double[matrix.size()][];
-    for (int i = 0; i<matrix.size(); ++i) {
+    for (int i = 0; i < matrix.size(); ++i) {
       matrixA[i] = matrix.get(i);
     }
     width = matrixA[0].length;
     height = matrixA.length;
   }
+
   /**
    * однопоточное умнджение матриц
    * должно поддерживаться для всех 4-х вариантов
@@ -45,11 +47,40 @@ public class DenseMatrix implements Matrix
    * @param o
    * @return
    */
-  @Override public Matrix mul(Matrix o)
-  {
-    return null;
-  }
+  @Override
+  public Matrix mul(Matrix o){
+    if (o instanceof DenseMatrix && matrixA[0].length == o.length)
 
+  {
+    double newArray = new double[o[0].length][];
+    double outArray = new double[matrixA.length][o[0].length];
+    double outArray1 = new double[o[0].length][matrixA.length];
+
+    for (int i = 0; i < o.length; ++i){
+      for (int j = 0; j < o[0].length; ++j){
+        newArray[j][i] = o[i][j];
+        outArray[i][j] = 0;
+      }
+    }
+
+    for (int i = 0; i < newArray.length ; ++i){
+      for (int j = 0; j < matrixA.length ; ++j){
+        for (int k = 0; k < matrixA[0].length ; ++k) {
+          outArray[i][j] += matrixA[j][k] * o[i][k]
+        }
+      }
+    }
+
+    for (int i = 0; i < o.length; ++i){
+      for (int j = 0; j < o[0].length; ++j){
+        outArray[j][i] = outArray1[i][j];
+      }
+    }
+
+    return outArray;
+  }
+    return null;
+}
   /**
    * многопоточное умножение матриц
    *
@@ -67,7 +98,22 @@ public class DenseMatrix implements Matrix
    * @return
    */
   @Override public boolean equals(Object o) {
+    if (o == matrixA) {
+      return true;
+    }
+
+    if (o instanceof DenseMatrix) {
+      if (matrixA.length != o.length || matrixA[0].length != o[0].length) {
+        return false;
+      }
+
+      for (int i = 0; i < matrixA.length ; ++i) {
+        if (matrixA[i] != o[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
     return false;
   }
-
 }
